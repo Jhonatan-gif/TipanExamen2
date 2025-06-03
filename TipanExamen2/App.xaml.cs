9f6ac2b2b9eef4cd6f2b1d138d27ba2d168cd8f7
jhonatanTipan <jhonatan.tipan@udla.edu.ec>
2025-06-03 07:22:16 -0500

    Agregar archivos de proyecto.

---
+++ TipanExamen2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TipanExamen2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
